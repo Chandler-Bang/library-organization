81a55e764949d795436ab1b98ff2a272ee3b8eed
aastargazer <63040389+aastargazer@users.noreply.github.com>
2020-06-15 23:04:14 +0800

    上架新书
    上架新书页面

---
+++ addnewbook.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html><head><meta name="GCD" content="YTk3ODQ3ZWZhN2I4NzZmMzBkNTEwYjJlb78b524241cf1f616a65bcfa8cc76fe4"/>
+  <meta charset="utf-8">
+  <title>shangjia</title>
+  <meta name="generator" content="Google Web Designer 8.0.3.0603">
+  <style type="text/css" id="gwd-text-style">p {
+    margin: 0px;
+}
+h1 {
+    margin: 0px;
+}
+h2 {
+    margin: 0px;
+}
+h3 {
+    margin: 0px;
+}</style>
+  <style type="text/css">html,
+body {
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+}
+body {
+    background-color: transparent;
+    transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+    -webkit-transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+    -moz-transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+    perspective: 1400px;
+    -webkit-perspective: 1400px;
+    -moz-perspective: 1400px;
+    transform-style: preserve-3d;
+    -webkit-transform-style: preserve-3d;
+    -moz-transform-style: preserve-3d;
+}
+.gwd-div-1cwg {
+    position: absolute;
+    width: 887px;
+    height: 108px;
+    left: 0px;
+    top: 0px;
+    background-image: none;
+    background-color: rgb(121, 35, 49);
+}
+.gwd-p-1erh {
+    position: absolute;
+    width: 592px;
+    text-align: center;
+    height: 81px;
+    transform-origin: 324.5px 32.0943px 0px;
+    -webkit-transform-origin: 324.5px 32.0943px 0px;
+    -moz-transform-origin: 324.5px 32.0943px 0px;
+    left: 128px;
+    top: 27px;
+}
+.gwd-span-69d4 {
+    color: rgb(240, 236, 237);
+    font-family: "Courier New";
+    font-size: 60px;
+}
+.gwd-form-auxk {
+    position: absolute;
+    height: 70%;
+    top: 25%;
+    width: 80%;
+    left: 10%;
+}
+.gwd-p-l5zy {
+    position: absolute;
+    left: 183px;
+    width: 88px;
+    top: 143px;
+    height: 25px;
+    transform-origin: 257.261px 24.197px 0px;
+    -webkit-transform-origin: 257.261px 24.197px 0px;
+    -moz-transform-origin: 257.261px 24.197px 0px;
+}
+.gwd-p-1pea {
+    position: absolute;
+    width: 85px;
+    height: 24px;
+    left: 185px;
+    top: 187px;
+}
+.gwd-p-12tw {
+    position: absolute;
+    width: 85px;
+    height: 32px;
+    transform-origin: 257.261px 24.197px 0px;
+    -webkit-transform-origin: 257.261px 24.197px 0px;
+    -moz-transform-origin: 257.261px 24.197px 0px;
+    left: 186px;
+    top: 223px;
+}
+.gwd-p-1fup {
+    position: absolute;
+    width: 88px;
+    height: 26px;
+    left: 185px;
+    top: 267px;
+}
+.gwd-p-vsrf {
+    position: absolute;
+    width: 88px;
+    height: 30px;
+    left: 185px;
+    top: 307px;
+}
+.gwd-p-oqdz {
+    position: absolute;
+    width: 89px;
+    height: 27px;
+    left: 185px;
+    top: 352px;
+}
+.gwd-p-dgyj {
+    position: absolute;
+    left: 189px;
+    width: 86px;
+    transform-origin: 245.827px 13.3501px 0px;
+    -webkit-transform-origin: 245.827px 13.3501px 0px;
+    -moz-transform-origin: 245.827px 13.3501px 0px;
+    top: 391px;
+    height: 25px;
+}
+.gwd-p-1cm8 {
+    position: absolute;
+    width: 91px;
+    height: 25px;
+    left: 185px;
+    top: 427px;
+}
+.gwd-input-18u3 {
+    position: absolute;
+    height: 26px;
+    left: 311px;
+    top: 145px;
+    width: 150px;
+}
+.gwd-input-1v0k {
+    position: absolute;
+    height: 28px;
+    left: 311px;
+    width: 150px;
+    top: 183px;
+}
+.gwd-input-zhv6 {
+    position: absolute;
+    height: 28px;
+    left: 311px;
+    width: 150px;
+    top: 223px;
+}
+.gwd-input-uw6i {
+    position: absolute;
+    height: 27px;
+    width: 150px;
+    left: 311px;
+    top: 265px;
+}
+.gwd-input-1jqa {
+    position: absolute;
+    height: 30px;
+    width: 150px;
+    left: 311px;
+    top: 305px;
+}
+.gwd-input-qbcu {
+    position: absolute;
+    width: 150px;
+    left: 311px;
+    top: 350px;
+    height: 25px;
+}
+.gwd-input-6y4l {
+    position: absolute;
+    height: 24px;
+    width: 150px;
+    left: 311px;
+    top: 390px;
+}
+.gwd-input-1b0l {
+    position: absolute;
+    height: 27px;
+    width: 150px;
+    left: 311px;
+    top: 423px;
+}
+.gwd-input-15fv {
+    position: absolute;
+    height: 31px;
+    left: 579px;
+    top: 425px;
+    width: 110px;
+}</style>
+</head>
+
+<body class="htmlNoPages">
+  <div class="gwd-div-1cwg"></div>
+  <p class="gwd-p-1erh"><span class="gwd-span-69d4">上 架 新 书</span><br>
+    
+  </p>
+  <form class="gwd-form-auxk"></form>
+  <p class="gwd-p-l5zy">图书编号</p>
+  <p class="gwd-p-1pea">图书名</p>
+  <p class="gwd-p-12tw">在馆状态</p>
+  <p class="gwd-p-1fup">图书状态</p>
+  <p class="gwd-p-vsrf">图书价格</p>
+  <p class="gwd-p-oqdz">上架时间</p>
+  <p class="gwd-p-dgyj">借阅频度</p>
+  <p class="gwd-p-1cm8">图书类别</p>
+  <input class="gwd-input-18u3" type="text" name="bookid">
+  <input class="gwd-input-1v0k" type="text" name="bookname">
+  <input class="gwd-input-zhv6" type="text" name="inlib">
+  <input class="gwd-input-uw6i" type="text" name="status">
+  <input class="gwd-input-1jqa" type="text" name="price">
+  <input class="gwd-input-qbcu" type="text" name="time">
+  <input class="gwd-input-6y4l" type="text" name="frequency">
+  <input class="gwd-input-1b0l" type="text" name="category">
+  <input class="gwd-input-15fv" type="submit">
+
+
+</body></html>
