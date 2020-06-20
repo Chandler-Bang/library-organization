a935014dd090c0c478d27126dbe5e77982613e00
QPPJY <qppambition@163.com>
2020-06-20 20:10:12 +0800

    记录罚款信息界面

--- finepage.php
+++ finepage.php
@@ -4,49 +4,130 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>图书分类</title>
+    <title>add new book</title>
     <style>
         * {
-            margin: 0;
-            padding: 0;
+            margin: 0px;
+            padding: 0px;
+        }
+
+        html,
+        body {
+            height: 100%;
+        }
+
+        h1 {
+            text-align: center;
+            background-color: red;
+        }
+
+        .box {
+            border: 5px solid;
+            border-radius: 10px;
+            top: 50px;
+            background-color: rgb(173, 169, 129);
+            margin: 0px auto;
+            width: 400px;
+            height: 500px;
+            position: relative;
         }
 
         .head {
-            margin-top: 50px;
             text-align: center;
-            text-decoration: underline;
-            text-underline-position: below;
+            font: 32px kaiti;
+            color: white;
+            background-color: brown;
+            font-weight: bold;
+            line-height: 40px;
+            padding: 10px;
+
         }
 
-        form {
-            text-align: center;
-            margin-top: 20px;
+        .profile {
+
+            color: white;
+            font: 20px kaiti;
+            margin: 5px 5px;
+        }
+
+        .submit {
+            position: absolute;
+            top: 430px;
+            left: 160px;
+        }
+
+        .text {
+            border: 3px;
+            height: 25px;
+            border-radius: 5px;
+            position: absolute;
+            left: 150px;
+            width: 220px;
+        }
+
+        .Title {
+            color: white;
+            font: 15px kaiti;
+            margin-left: 20px;
+
         }
 
         .button {
-            border: none;
+            margin: 0 auto;
+            padding: 8px;
             background-color: #892844;
-            width: 150px;
-            padding: 10px 10px;
             color: white;
-            margin-top: 20px
+            width: 90px;
+            border-radius: 6px;
         }
 
-        .select {
-            margin: 0 50px;
+        .button:hover {
+            background-color: rgb(62, 12, 85);
+        }
+
+        .back {
+            position: absolute;
+            top: 95%;
+            left: 10px;
+        }
+
+        a {
+            text-decoration: none;
+            background-color: darkgoldenrod;
+            color: white;
+            border-radius: 2px;
+            padding: 5px;
         }
     </style>
 </head>
 
+
 <body>
-    <div class="head">记录罚款信息</div>
-    <form method="POST" action="fine.php">
-	            请输入流水号：<input name='serialnumber' type='text'/></br>
-				请进行违约备注：<input name='note' type='text'/></br>
-				请输入罚款金额：<input name='fine' type='text'/></br>
-		<input class="button" type="submit" name="insert" value="登记罚款"></br>
-    </form>
-	<a href='allfunction.php' target='finepage.php'><center>返回主页面</center></a></br>
+    <div class="box">
+        <div class="head">记录罚款信息</div>
+        <br>
+        <div class="profile">请输入图书相关信息:</div><br>
+        <form method="POST" action="fine.php">
+		
+            <div>
+                <span class="Title" style="letter-spacing: 0.2em;">请输入流水号</span><input class="text" type="text"name="serialnumber"><br><br>
+            </div>
+			
+            <div>
+                <span class="Title" style="letter-spacing: 0.2em;">请进行违约备注</span><input class="text" type="text" name="note"><br><br>
+            </div>
+			
+			<div >
+                <span class="Title" style="letter-spacing: 0.2em;">请输入罚款金额</span><input class="text" type="text"
+                    name="fine"><br><br>
+            </div>
+			
+		
+			
+            <div class="submit"><input class="button" type="submit" name="button" value="提交"></div>
+        </form>
+    </div>
+    <div class="back"><a href="main.php">返回主界面</a></div>
 </body>
 
 </html>
