87b446a5b5b1be9e6acd7c4a6ff069f72477aaa7
zhou <1032941267@qq.com>
2020-06-20 15:54:13 +0800

    登录界面修复

--- validate.php
+++ validate.php
@@ -1,17 +1,20 @@
 <?php
 $admin = $_REQUEST['adminname'];
 $password = $_REQUEST['password'];
+if($admin=="" || $password==""){
+    echo '<script type="text/javascript">alert("用户名或密码不能为空");location="index.php";</script>';
+}
 $mysqli = mysqli_connect("localhost", "root", "", "library");
 if (mysqli_connect_errno()) {
     die("Failed to connect to MySQL: (" . mysqli_connect_errno() . ") " . mysqli_connect_error());
-} else
-    echo 'successful to connect to MySQL!<br/>';
+} 
 $demand = "select password from Admin where adminname = '$admin'";
 $res = mysqli_query($mysqli, $demand);
 $power = mysqli_fetch_object($res);
-echo "$power->password";
 if ($power->password == $password) {
     header('location:main.php');
-} else header('location:login.php');
+} else {
+    echo '<script type="text/javascript">alert("用户名或密码错误，请重新登陆");location="index.php";</script>';
+}
 mysqli_free_result($res);
 mysqli_close($mysqli);
