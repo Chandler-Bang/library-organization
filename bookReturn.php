e7d33a4a8d4abab5f2d0f10214c8365a55c2b139
zhou <1032941267@qq.com>
2020-06-23 15:10:53 +0800

    重写还书逻辑

--- bookReturn.php
+++ bookReturn.php
@@ -2,6 +2,7 @@
 $serialNum=$_REQUEST['serialNumber'];
 $readerNo=$_REQUEST['readerNo'];
 $bookNo=$_REQUEST['bookNo'];
+$lossResult = $_REQUEST['lossResult'];
 date_default_timezone_set('Asia');//设置为北京时间
 $returnDate=date("Y-m-d");//自动获取时间(年月日)
 
@@ -16,12 +17,26 @@ if(!$checkQuery){
     mysqli_close($mysqli); // 图书不存在，停止查询
     echo '<script>alert("抱歉，此图书可能不存在");location="returnpage.php";</script>';
 }
-//插入书籍归还信息命令
-$demand = "INSERT INTO returnBook values($serialNum,'$readerNo','$bookNo','$returnDate')";
-$res=mysqli_query($mysqli,$demand); //执行还书的操作 -- 在returnBook表中插入新行
-$demand2 = "UPDATE book set bookStatus='normal',inLibStatus='available' where bookNo = '$bookNo'";
-if ($res) {
-    mysqli_query($mysqli,$demand2);
+// 第二步: 锁定图书状态，防止出现冲突
+$locker = "UPDATE book set inLibStatus='locked' where bookNo='$bookNo'";
+$lockQUery = mysqli_query($mysqli,$locker);
+if(!$lockQUery){
+    mysqli_close($mysqli);
+    echo '<script>alert("图书锁定失败，请联系数据库维护人员");location="borrowpage.php";</script>';
+}
+// 第三步: 判断图书是否造成损失，如果是，登记损失
+$inLibStatus = ($lossResult=='normal')?'available':'unavailable';
+if($inLibStatus!='available'){
+    $lossRecord = "INSERT INTO loss values('$bookNo','$lossResult''$recordDate')";
+    mysqli_query($mysqli,$lossRecord);
+}
+// 第四步: 登记归还记录
+$returnRecord = "INSERT INTO returnBook values($serialNum,'$readerNo','$bookNo','$returnDate')";
+mysqli_query($mysqli,$returnRecord);
+// 第五步: 修改图书状态，解除锁定
+$bookStatus = "UPDATE book SET inLibStatus='inLibStatus' where bookNo = '$bookNo'";
+$res = mysqli_query($mysqli,$bookStatus);
+if($res){
     mysqli_close($mysqli);
     echo '<script>
             var r = confirm("还书结果录入完毕，是否跳转到罚款界面？");
