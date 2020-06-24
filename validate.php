bb5499a9f5d8a6ee0a61eb88035a08abb21054af
zhou <1032941267@qq.com>
2020-06-24 15:45:20 +0800

    登录认证修正

--- validate.php
+++ validate.php
@@ -3,16 +3,25 @@ $admin = $_REQUEST['adminname'];
 $password = $_REQUEST['password'];
 if($admin=="" || $password==""){
     echo '<script type="text/javascript">alert("用户名或密码不能为空");location="index.php";</script>';
+    exit;
 }
 $mysqli = mysqli_connect("localhost", "root", "", "library");
 if (mysqli_connect_errno()) {
     die("Failed to connect to MySQL: (" . mysqli_connect_errno() . ") " . mysqli_connect_error());
 } 
-$demand = "select password from Admin where adminname = '$admin'";
+$demand = "select password,level from Admin where adminname = '$admin'";
 $res = mysqli_query($mysqli, $demand);
-$power = mysqli_fetch_object($res);
-if ($power->password == $password) {
-    header('location:main.php');
+$power = mysqli_fetch_row($res);
+if ($power[0]== $password) {
+    if($power[1]=='normal'){
+        header('location:main.php');
+    } else if($power[1]=='super'){
+        header('location:main.su.php');
+    }
+    else {
+        echo '<script type="text/javascript">alert("未知错误，请联系管理员");location="index.php";</script>';
+        exit;
+    }
 } else {
     echo '<script type="text/javascript">alert("用户名或密码错误，请重新登陆");location="index.php";</script>';
 }
