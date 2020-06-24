3b619e7f631314b9f847043b251c9d303c326f33
zhou <1032941267@qq.com>
2020-06-24 14:51:47 +0800

    查询界面增加流水号的显示

--- Rborr_historyS.php
+++ Rborr_historyS.php
@@ -57,16 +57,16 @@ $id=$_POST['readerno'];//接收id，为读者输入的读者号
 
 $mysqli = mysqli_connect("localhost", "root", "", "library");
 echo"<div class='head'><br>查询结果</div>";
-	$str="select book.bookNo,book.bookName,reader.readerNo,reader.readerName,borrowDate,returnDate from book left join borrowbook on book.bookNo=borrowbook.bookNo left join returnbook on borrowbook.bookNo=returnbook.bookNo left join reader on borrowbook.readerNo=reader.readerNo where reader.readerNo='$id' ";  //按读者号查询某图书的借阅历史
+	$str="SELECT book.bookNo,book.bookName,reader.readerNo,reader.readerName,borrowDate,returnDate,borrowbook.serialNumber FROM book left join borrowbook ON book.bookNo=borrowbook.bookNo left join returnbook on borrowbook.bookNo=returnbook.bookNo LEFT JOIN reader ON borrowbook.readerNo=reader.readerNo WHERE reader.readerNo='$id' ";  //按读者号查询某图书的借阅历史
 	echo "<table border='4px' cellpadding='15px' cellspacing='0px'>";
 
-echo"<tr><td>图书编号</td><td>图书名</td><td>读者号</td><td>读者名</td><td>借阅日期</td><td>还书日期</td></tr>";
+echo"<tr><td>图书编号</td><td>图书名</td><td>读者号</td><td>读者名</td><td>借阅日期</td><td>还书日期</td><td>流水号</td></tr>";
 
 if($res=mysqli_query($mysqli,$str))  //这个和mysql_query参数有一定的顺序差别，这个用不了反过来就行
 {
 	while($row=mysqli_fetch_row($res))
     {
-        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td></tr>";
+        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td></tr>";
     }
 }
 echo "</table>";
