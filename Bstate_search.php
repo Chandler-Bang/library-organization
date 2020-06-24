e1140d99a4a27ce6650198b108d2939e7b33c1de
zhou <1032941267@qq.com>
2020-06-24 20:49:16 +0800

    增加了两列查询结果

--- Bstate_search.php
+++ Bstate_search.php
@@ -21,7 +21,7 @@
 		.head {
 			text-align: center;
 			font-family: Microsoft Yahei;
-			font-size: 90px;
+			font-size: 60px;
 			font-weight: bold;
 			color: white;
 			height: 40%;
@@ -51,7 +51,7 @@
 
 	<?php
 	$id = $_REQUEST['type']; //接收id，值分别为bookname,bookid(bookNo)
-	$bookNo = $_REQUEST['bookNo'];
+	$bookInfo = $_POST['bookNo'];
 	$conn = mysqli_connect('localhost', 'root', '');
 	mysqli_select_db($conn, 'library');
 	// 开始输出表格
@@ -59,18 +59,17 @@
 	echo "<table border='4px' cellpadding='15px' cellspacing='0px'>";
 
 	if ($id == 'bookid') {
-		$str = "select bookNo,bookName,inLibStatus from book where bookNo='$bookNo' "; //按编号从书单中查询书籍状态
-		echo "<tr><td>图书编号</td><td>图书名</td><td>在馆状态</td></tr>";
+		$str = "select bookNo,bookName,inLibStatus,bookPrice,category from book where bookNo='$bookInfo' "; //按编号从书单中查询书籍状态
 	}
 	if ($id == 'bookname') {
-		$str = "select bookName,bookNo,inLibStatus from book where bookName like '%$bookNo%'";
-		echo "<tr><td>图书名</td><td>图书编号</td><td>在馆状态</td></tr>"; //按书名从书单中查询书籍状态
+		$str = "select bookName,bookNo,inLibStatus,bookPrice,category from book where bookName like '%$bookInfo%'";
 	}
 	if ($res = mysqli_query($conn, $str))  //这个和mysql_query参数有一定的顺序差别，这个用不了反过来就行
 	{
 
+		echo "<tr><td>图书名</td><td>图书编号</td><td>在馆状态</td><td>图书价格</td><td>图书种类</td></tr>"; 
 		while ($row = mysqli_fetch_row($res)) {
-			echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+			echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td></tr>";
 		}
 	}
 
