51e307a647f249d287c7506899027cb2d4e9fa4a
aastargazer <1580249881@qq.com>
2020-06-24 13:00:27 +0800

    读者历史、图书历史、图书状态、推荐结果页面的修改

--- Rborr_historyS.php
+++ Rborr_historyS.php
@@ -1,11 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>图书在馆状态</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+		
+        html,body 
+		{
+            height: 100%;
+			background-color:rgb(173, 169, 129);
+        }
+		
+		
+		.head
+		{
+			text-align: center;
+			font-family:仿宋;
+		  font-size:90px;
+		  font-weight:bold;
+		 color:white;
+		 height:40%;
+		}
+		
+		table
+		{
+			text-align:center;
+			margin: 0px auto;
+		    border:5px double;
+			
+			width:35%;
+		}
+      
+	  a
+	  {
+		  color:white;
+		  font-size:20px;
+	  }
+	  
+	  .bottom
+	  {
+	text-align:center;
+	  }
+    </style>
+</head>
+
+<body>
+
 <?php
 $id=$_POST['readerno'];//接收id，为读者输入的读者号
 
 $mysqli = mysqli_connect("localhost", "root", "", "library");
-
+echo"<div class='head'><br>查询结果</div>";
 	$str="select book.bookNo,book.bookName,reader.readerNo,reader.readerName,borrowDate,returnDate from book left join borrowbook on book.bookNo=borrowbook.bookNo left join returnbook on borrowbook.bookNo=returnbook.bookNo left join reader on borrowbook.readerNo=reader.readerNo where reader.readerNo='$id' ";  //按读者号查询某图书的借阅历史
 	echo "<table border='4px' cellpadding='15px' cellspacing='0px'>";
-echo"<tr align='center'>查询结果</tr>";
+
 echo"<tr><td>图书编号</td><td>图书名</td><td>读者号</td><td>读者名</td><td>借阅日期</td><td>还书日期</td></tr>";
 
 if($res=mysqli_query($mysqli,$str))  //这个和mysql_query参数有一定的顺序差别，这个用不了反过来就行
@@ -16,7 +70,9 @@ if($res=mysqli_query($mysqli,$str))  //这个和mysql_query参数有一定的顺
     }
 }
 echo "</table>";
-echo "<a href='Rborr_history.php' >返回上一页面</a></br>";
-echo "<a href='main.php'>回到首页</a></br>";
+echo "<div class='bottom'><br><a href='Rborr_history.php' >返回上一页面</a></br>";
+echo "<a href='main.php'><br>回到首页</a></br></div>";
 mysqli_close($mysqli);  //还差美化界面
 ?>
+</body>
+</html>
--- recommend_backend.php
+++ recommend_backend.php
@@ -1,3 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>图书在馆状态</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+		
+        html,body 
+		{
+            height: 100%;
+			background-color:rgb(173, 169, 129);
+        }
+		
+		
+		.head
+		{
+			text-align: center;
+			font-family:仿宋;
+		  font-size:90px;
+		  font-weight:bold;
+		 color:white;
+		 height:40%;
+		}
+		
+		table
+		{
+			text-align:center;
+			margin: 0px auto;
+		    border:5px double;
+			
+			width:35%;
+		}
+      
+	  a
+	  {
+		  color:white;
+		  font-size:20px;
+	  }
+	  
+	  .bottom
+	  {
+	text-align:center;
+	  }
+    </style>
+</head>
+
+
+<body>
+
 <?php
 $type=$_POST['type']; // 搜索类型传入本页面
 $mysqli = mysqli_connect("localhost", "root", "", "library");
@@ -10,13 +65,13 @@ echo "<table border='4px' cellpadding='15px' cellspacing='0px'>";
 if($type=='hot')
 {
 	$str="select bookName,category,frequency from book order by frequency DESC limit 10";//排名前十的图书
-	echo"<tr><td align='center'>热书书单</td></tr>";
+	echo"<div class='head'><br>热书书单</div>";
 	echo"<tr><td>图书名</td><td>图书类别</td><td>频度</td></tr>";
 }
 if($type=='latest')
 {
 	$str="select bookName,category,importDate from book order by importDate";
-	echo"<tr align='center'>新书书单</tr>";
+	echo"<div class='head'><br>新书书单</div>";
 	echo"<tr><td>图书名</td><td>图书类别</td><td>上架时间</td></tr>";
 }
 if($res=mysqli_query($mysqli,$str))
@@ -29,7 +84,9 @@ if($res=mysqli_query($mysqli,$str))
 
 echo "</table>";
 // 表格输出完毕
-echo "<a href='recommend.php' >返回上一页面</a></br>";
-echo "<a href='main.php' >回到首页</a></br>";
+echo "<div class='bottom'><br><a href='recommend.php' >返回上一页面</a></br>";
+echo "<a href='main.php' ><br>回到首页</a></br></div>";
 mysqli_close($mysqli);  //还差美化界面 -> TODO: 等测试阶段进行
 ?>
+</body>
+</html>
